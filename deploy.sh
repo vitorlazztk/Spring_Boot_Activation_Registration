cff4aaa7ae6be66f93ad785325446e89f485536c
ZVARIST <istvan.varro@partner.qualysoft.com>
2020-01-05 21:52:45 +0100

    deploy file-k

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /root/java/Spring_Boot_Database_login
+echo Leszedem a GIT-ről a legfrisebb verziót
+git pull
+echo Kezdem a buildelést!
+mvn clean install
+echo Buildelés vége!
+echo jar másolása
+mv target/sec-0.1.jar ./
+echo Leállítom az aktuális java alkalmazást
+pkill java
+echo Indítom az új verziót
+nohup java -jar sec-0.1.jar &
